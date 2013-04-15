61ba16166343657a44cf1101baf2df50f06838ca
Forexpert Meng <forexpert@yeah.net>
2013-04-15 18:52:52 +0800

    add an service to do periodical trade to extend the demo accounts

---
+++ forex/broker-client/src/main/java/com/mengruojun/brokerclient/dukascopy/utils/account/ExtendDemoAccountStrategy.java
@@ -0,0 +1,98 @@
+package com.mengruojun.brokerclient.dukascopy.utils.account;
+
+import com.dukascopy.api.IAccount;
+import com.dukascopy.api.IBar;
+import com.dukascopy.api.IConsole;
+import com.dukascopy.api.IContext;
+import com.dukascopy.api.IEngine;
+import com.dukascopy.api.IIndicators;
+import com.dukascopy.api.IMessage;
+import com.dukascopy.api.IOrder;
+import com.dukascopy.api.IStrategy;
+import com.dukascopy.api.ITick;
+import com.dukascopy.api.Instrument;
+import com.dukascopy.api.JFException;
+import com.dukascopy.api.Period;
+import com.mengruojun.brokerclient.dukascopy.utils.DukascopyUtils;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
+/**
+ * This is a Dukascopy extendDemoAccountStrategy. It will open a position and close it when it starts, and will do it again once a week(at Monday 00:00:00);
+ * So that we can use the demo account without expiration.
+ */
+@Service("extendDemoAccountStrategy")
+public class ExtendDemoAccountStrategy implements IStrategy {
+  SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss Z");
+  boolean firstTrade = false;
+
+  {
+    sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+  }
+
+  private IEngine engine = null;
+  private IContext context = null;
+  private IIndicators indicators = null;
+  private int tagCounter = 0;
+  private double[] ma1 = new double[Instrument.values().length];
+  private IConsole console;
+  Logger logger = Logger.getLogger(this.getClass());
+  private List<Instrument> dukascopyInstrumentList = DukascopyUtils.getInterestInstrumentList();
+
+
+  public void onStart(final IContext context) throws JFException {
+    this.context = context;
+    engine = context.getEngine();
+    indicators = context.getIndicators();
+    this.console = context.getConsole();
+    console.getOut().println("Started");
+
+  }
+
+
+  public void onStop() throws JFException {
+    for (IOrder order : engine.getOrders()) {
+      order.close();
+    }
+    console.getOut().println("Stopped");
+  }
+
+  IOrder order = null;
+
+  public void onTick(Instrument instrument, ITick tick) throws JFException {
+  }
+
+  public void onBar(Instrument instrument, Period period, IBar askBar, IBar bidBar) throws JFException {
+    if (instrument.equals(Instrument.EURUSD)) {
+      if (!firstTrade || period.equals(Period.WEEKLY)) {
+        doMinimumTrade();
+      }
+      closeOrders(engine.getOrders());
+
+    }
+  }
+
+  private void closeOrders(List<IOrder> orders) throws JFException {
+    for(IOrder o: orders){
+      if(o.getState() == IOrder.State.FILLED || o.getState() == IOrder.State.OPENED){
+        o.close();
+      }
+    }
+  }
+
+  private void doMinimumTrade() throws JFException {
+    order = engine.submitOrder("extend_" + new Date().getTime(), Instrument.EURUSD, IEngine.OrderCommand.SELL, 0.001, 0, 0);
+  }
+
+  public void onMessage(IMessage message) throws JFException {
+  }
+
+  public void onAccount(IAccount account) throws JFException {
+    //registerClient();
+  }
+}
